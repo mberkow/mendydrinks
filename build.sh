965a4738160aa97305db0e2625ddbb3443e24ed6
Mendy Berkowitz <mberkow@gmail.com>
2017-11-08 10:45:19 -0500

    For some reason we need to turn on cloudfront ability in the aws cli

--- build.sh
+++ build.sh
@@ -28,5 +28,7 @@ hugo -v
 # Sync
 aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME --exclude 'post'
 
+# in the codebuild environment we need to set this to gain access
+aws configure set preview.cloudfront true
 # Invalidate
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html / /page/*
